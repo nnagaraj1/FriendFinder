4a199be7403ca90c6d224a8aa58b00cc572c898c
Neeni Nagaraj <neeninagaraj@neenis-air.attlocal.net>
2018-05-21 11:55:33 -0400

    update working

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,20 +1,56 @@
+// Dependencies
+var path = require("path");
+////var friends = require ("/Users/neeninagaraj/Desktop/FriendFinder/app/data/friends.js");
+var friends=require("../app/data/friends.js");
+
 // Displays a JSON of all possible friends
-app.get("/api/friends", function(req, res) {
-    return res.json(friends);
+module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+       res.json(friends);
   })
 
-// Create New Characters - takes in JSON input
+// New Friend Entries - takes in JSON input
 app.post("/api/friends", function(req, res) {
+  console.log(req.body)
+  var bestfriend={
+    name: "",
+    photo: "",
+    friendDifference: Infinity
+  }
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     var newFriend = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newFriend
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newFriend);
-  
-    characters.push(newFriend);
-  
-    res.json(newFriend);
+    var newFriendResponses = newFriend.scores;
+    var totdiff;//
+
+
+// // Goes through current friend list   
+for (var i = 0; i < friends.length; i++) {
+var currentfrnd=friends[i];
+totdiff=0;
+//   // Sorts new friends' scores and compares current and new lists
+ for (var j = 0; j < currentfrnd.scores.length; j++) {
+  var currentFriendScore = currentfrnd.scores[j];
+  var currentUserScore = newFriendResponses[j];
+
+  // We calculate the difference between the scores and sum them into the totalDifference
+  totdiff += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+  }
+//   // Results go to friendScores array
+  // If the sum of differences is less then the differences of the current "best match"
+  if (totdiff <= bestfriend.friendDifference) {
+    // Reset the bestfriend  to be the new friend.
+    bestfriend .name = currentfrnd.name;
+    bestfriend .photo = currentUserScore.photo;
+    bestfriend .friendDifference = totdiff;
+  }
+}
+
+// Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
+// the database will always return that the user is the user's best friend).
+friends.push(newFriend);
+
+// Return a JSON with the user's bestfriend . This will be used by the HTML in the next page
+res.json(bestfriend );
   });
+}
--- server.js
+++ server.js
@@ -7,15 +7,25 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT||8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 
+app.use('/static', express.static(path.join(__dirname, 'app/public')))
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("FriendFinder app listening on PORT " + PORT);
   });
   
